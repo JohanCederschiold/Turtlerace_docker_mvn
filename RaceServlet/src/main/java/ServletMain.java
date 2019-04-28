33e372b984dc72fbf588b82ab1806776585c8405
Vagrant Default User <vagrant@stretch.localdomain>
2019-04-28 06:56:01 +0000

    Added cors

--- RaceServlet/src/main/java/ServletMain.java
+++ RaceServlet/src/main/java/ServletMain.java
@@ -40,8 +40,13 @@ public class ServletMain extends HttpServlet {
 	
 	
 	
+	
+	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		response.setHeader("Access-Control-Allow-Origin", "*");
+//	    response.setHeader("Access-Control-Allow-Methods", "GET");
 		
 		String userRequest = request.getParameter("get");
 		
@@ -62,6 +67,11 @@ public class ServletMain extends HttpServlet {
 		}
 		
 		
+	}
+	
+	@Override
+	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	    response.setHeader("Access-Control-Allow-Origin", "*");
 	}
 	
 	
